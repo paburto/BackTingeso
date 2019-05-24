1d25d445c846b9e3e3416945a3484b6a0488c401
Unknown <liliana.huallcca@usach.cl>
2019-05-24 13:45:18 -0400

    Controlador habitaciones casi terminado

--- src/main/java/MingesoTingeso/demo/Controllers/HabitacionController.java
+++ src/main/java/MingesoTingeso/demo/Controllers/HabitacionController.java
@@ -19,4 +19,85 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import MingesoTingeso.demo.Models.Habitacion;
-import MingesoTingeso.demo.Repositories.ProductoRepository;
+import MingesoTingeso.demo.Repositories.HabitacionRepository;
+
+//Evaluacion 07-mayo-2019
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/habitaciones")
+public class HabitacionController {
+	@Autowired
+	HabitacionRepository habitacionRepository;
+
+	@RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<Habitacion> getAllHabitaciones() {
+        return habitacionRepository.findAll();
+    }
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Habitacion getHabitacionById(@PathVariable Long id) {
+        return habitacionRepository.findHabitacionByIdHab(id);
+    }
+
+		@PostMapping("/update/{id}")
+			@ResponseBody
+			public List<HashMap<String, String>> update(@PathVariable Long id, @RequestBody Map<String, Object> jsonData) throws ParseException {
+			List<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
+			HashMap<String, String> map = new HashMap<>();
+			Habitacion habitacion = habitacionRepository.findHabitacionByIdHab(id);
+			System.out.println("hola");
+			if(habitacion == null) {
+				map.put("status", "404");
+				map.put("message", "Product does not exist!.");
+				map.put("item", "");
+				result.add(map);
+				return result;
+			}
+
+			else {
+				habitacion.setTipoHabitacion(jsonData.get("tipo").toString());
+				habitacion.setCapacidadNinos(Integer.parseInt(jsonData.get("capacidadNinos").toString()));
+				habitacion.setCapacidadAdultos(Integer.parseInt(jsonData.get("capacidadAdultos").toString()));
+				habitacion.setPrecioNoche(Integer.parseInt(jsonData.get("precioNoche").toString()));
+				habitacionRepository.save(habitacion);
+				map.put("status", "200");
+				map.put("message", "OK");
+				result.add(map);
+				return result;
+			}
+		}
+
+
+
+	@RequestMapping(value = "/precio/{precio}", method = RequestMethod.GET)
+    @ResponseBody
+    public Habitacion getHabitacionByPrecio(@PathVariable int precioNoche) {
+        return habitacionRepository.findHabitacionByPrecioNoche(precioNoche);
+    }
+
+	@CrossOrigin(origins = "*")
+	@PostMapping("/delete/{id}")
+    @ResponseBody
+    public List<HashMap<String, String>> update(@PathVariable Long id) throws ParseException {
+		List<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
+		HashMap<String, String> map = new HashMap<>();
+		Habitacion habitacion = habitacionRepository.findHabitacionByIdHab(id);
+		if(habitacion == null) {
+			map.put("status", "404");
+			map.put("message", "Product does not exist!.");
+			map.put("item", "");
+			result.add(map);
+			return result;
+		}
+		else {
+			habitacionRepository.deleteById(id);
+			map.put("status", "200");
+			map.put("message", "OK, Habitacion erased!.");
+			result.add(map);
+			return result;
+		}
+    }
+}
---
+++ src/main/java/MingesoTingeso/demo/Repositories/HabitacionRepository.java
@@ -0,0 +1,22 @@
+package MingesoTingeso.demo.Repositories;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import MingesoTingeso.demo.Models.Habitacion;
+
+public interface HabitacionRepository extends JpaRepository<Habitacion, Long>{
+
+Habitacion findHabitacionByIdHab(Long idHab);
+
+Habitacion findHabitacionByTipo(String tipo);
+
+Habitacion findHabitacionByCapacidadNinos(int capacidadNinos);
+
+Habitacion findHabitacionByCapacidadAdultos(int capacidadAdultos);
+
+Habitacion findHabitacionByPrecioNoche(int precioNoche);
+
+Habitacion deleteHabitacionByIdHab(Long idHab);
+
+}
