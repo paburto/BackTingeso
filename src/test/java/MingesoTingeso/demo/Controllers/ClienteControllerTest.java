8460e4e917502cac91b7987735bd51c202783aa1
Edgar Blau <edgar.blau@usach.cl>
2019-05-30 15:58:09 -0400

    Test de jUnit controlador.

--- src/test/java/MingesoTingeso/demo/Controllers/ClienteControllerTest.java
+++ src/test/java/MingesoTingeso/demo/Controllers/ClienteControllerTest.java
@@ -3,23 +3,19 @@ package MingesoTingeso.demo.Controllers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-import java.util.List;
-import MingesoTingeso.demo.Models.Cliente;
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith( SpringJUnit4ClassRunner.class )
 @ContextConfiguration
-@SpringBootTest
+@WebMvcTest(ClienteController.class)
 public class ClienteControllerTest {
     @Autowired
     ClienteController cc;
@@ -28,17 +24,15 @@ public class ClienteControllerTest {
    
     @Test
     public void getAllClientes() throws Exception {
-    	this.mockMvc.perform(get("/clientes")
-    	           .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
-    	           .andExpect(status().isOk())
-    	           .andExpect(content().contentType("application/json"));
+    	this.mockMvc.perform(get("/clientes").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json"));
     }
 
     @Test
     public void getNombreClienteById() throws Exception {
-    	this.mockMvc.perform(get("/clientes/1")
- 	           .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
- 	           .andExpect(status().isOk())
- 	           .andExpect(content().contentType("application/json"));
+    	this.mockMvc.perform(get("/clientes/1").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json"));
     }
 }
