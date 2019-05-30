dccbb40b7ab33d9c1a4211a4a8ac226ac52b0bb2
Vicente <vicente.brevis@usach.cl>
2019-05-30 14:29:33 -0400

    Se modifican dependencias para java mail

--- src/main/java/MingesoTingeso/demo/Controllers/FeedbackController.java
+++ src/main/java/MingesoTingeso/demo/Controllers/FeedbackController.java
@@ -1,20 +1,21 @@
-package MingesoTingeso.demo.Services;
+package MingesoTingeso.demo.Controllers;
 
 import MingesoTingeso.demo.configFIles.EmailCfg;
 import MingesoTingeso.demo.Services.Feedback;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.xml.bind.ValidationException;
+import javax.validation.ValidationException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("feedback")
-
+@RequestMapping("/feedback")
 public class FeedbackController {
 
     private EmailCfg emailCfg;
@@ -24,28 +25,35 @@ public class FeedbackController {
     }
 
     @PostMapping
-    public void seendFeedback(@RequestBody Feedback feedback, BindingResult bindingResult) throws ValidationException {
-        if (bindingResult.hasErrors()){
-            throw new ValidationException("Feedback no es válido");
+    public List<HashMap<String, String>> sendFeedback(@RequestBody Feedback feedback,
+                             BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new ValidationException("Feedback is not valid");
         }
 
+        // Crendo un  mail sender
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(this.emailCfg.getHost());
+        mailSender.setPort(this.emailCfg.getPort());
+        mailSender.setUsername(this.emailCfg.getUsername());
+        mailSender.setPassword(this.emailCfg.getPassword());
+
+        // Creando un email instance
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(feedback.getEmail());
+        mailMessage.setTo("vicent.brevis@gmail.com");
+        mailMessage.setSubject("New feedback from " + feedback.getName());
+        mailMessage.setText(feedback.getFeedback());
+
+        // Enviando email
+        mailSender.send(mailMessage);
+
+        List<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "200");
+        map.put("message", "OK.");
+        result.add(map);
+        return result;
+
     }
-
-    //Creando Mail Sender
-    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-    mailSender.setHost(this.emailCfg.getHost());
-    mailSender.setPort(this.emailCfg.getPort());
-    mailSender.setUsername(this.emailCfg.getUsername());
-    mailSender.setPassword(this.emailCfg.getPassword());
-
-    // Creando instancia de Mail
-    SimpleMailMessage mailMessage = new SimpleMailMessage();
-    mailMessage.setFrom(feedback.getEmail());
-    mailMessage.setTo("rc@feedback.com");
-    mailMessage.setSubject("New feedback from " + feedback.getName());
-    mailMessage.setText(feedback.getFeedback());
-
-    // Send mail
-        mailSender.send(mailMessage)
-
 }
