a7af6f509d3411c782256ac68a8bd356e89cb296
Edgar Blau <edgar.blau@usach.cl>
2019-05-30 16:02:18 -0400

    Testing de jUnit.

--- src/test/java/MingesoTingeso/demo/Controllers/ClienteControllerTest.java
+++ src/test/java/MingesoTingeso/demo/Controllers/ClienteControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith( SpringJUnit4ClassRunner.class )
 @ContextConfiguration
+@SpringBootTest
 @WebMvcTest(ClienteController.class)
 public class ClienteControllerTest {
     @Autowired
