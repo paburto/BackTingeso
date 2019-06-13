a5a80925dfad324e0cda4b38e3b28eed51199749
Diego Águila <diegoaguilatorneria@gmail.com>
2019-06-12 21:42:27 -0400

    fix

--- src/main/java/MingesoTingeso/demo/Repositories/ResHabRepository.java
+++ src/main/java/MingesoTingeso/demo/Repositories/ResHabRepository.java
@@ -2,12 +2,12 @@ package MingesoTingeso.demo.Repositories;
 import java.util.List;
 
 import MingesoTingeso.demo.Models.Habitacion;
+import MingesoTingeso.demo.Models.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import MingesoTingeso.demo.Models.ReservaHabitacion;
 
 public interface ResHabRepository extends JpaRepository<ReservaHabitacion, Long>{
 	List<ReservaHabitacion> findReservaHabitacionByHabitacion(Habitacion habitacion);
-	ReservaHabitacion findReservaHabitacionByReserva(Long idReserva);
-	List<ReservaHabitacion> findByIdReserva(Long idReserva);
+	List<ReservaHabitacion> findReservaHabitacionByReserva(Reserva reserva);
 }
