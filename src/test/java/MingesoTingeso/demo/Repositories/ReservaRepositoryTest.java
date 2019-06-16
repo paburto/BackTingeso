c546aff7efb28af805ed560b89ec38a3e3995dd0
Vicente <vicente.brevis@usach.cl>
2019-06-16 03:05:22 -0400

    Se agrega test ReservaRepository

--- src/test/java/MingesoTingeso/demo/Repositories/ReservaRepositoryTest.java
+++ src/test/java/MingesoTingeso/demo/Repositories/ReservaRepositoryTest.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-/*
+
 import MingesoTingeso.demo.Models.Reserva;
 @RunWith( SpringJUnit4ClassRunner.class )
 @ContextConfiguration
@@ -19,38 +20,41 @@ public class ReservaRepositoryTest {
 	@Autowired
 	ReservaRepository reservaRepository;
 
+    @Autowired
+    List<Reserva> lr;
+
+	@Before
+    public void before(){
+        List<Reserva> lr = reservaRepository.findAll();
+    }
+
 	@Test
 	public void findReservaByIdReserva() {
-		List<Reserva> lr = reservaRepository.findAll();
 		long id = lr.get(0).getIdReserva();
 		assertEquals(id, (long)reservaRepository.findReservaByIdReserva(id).getIdReserva());
 	}
 
 	@Test
 	public void findReservaByDescuento() {
-		List<Reserva> lr = reservaRepository.findAll();
 		int descuento = lr.get(0).getDescuento();
 		assertEquals(descuento, reservaRepository.findReservaByDescuento(descuento).get(0).getDescuento());
 	}
 
 	@Test
 	public void findReservaByEstado() {
-		List<Reserva> lr = reservaRepository.findAll();
 		int estado = lr.get(0).getEstado();
 		assertEquals(estado, reservaRepository.findReservaByEstado(estado).get(0).getEstado());
 	}
 	
 	@Test
 	public void findReservaByCodigoReserva(){
-		List<Reserva> lr = reservaRepository.findAll();
 		int codigo = lr.get(0).getCodigoReserva();
 		assertEquals(codigo, reservaRepository.findReservaByCodigoReserva(codigo).getCodigoReserva());
 	}
 
 	@Test
 	public void getReservaByCodigoReserva(){
-		List<Reserva> lr = reservaRepository.findAll();
 		int codigo = lr.get(0).getCodigoReserva();
 		assertEquals(codigo, reservaRepository.getReservaByCodigoReserva(codigo).getCodigoReserva());
 	}
-}*/
+}
