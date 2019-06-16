3917fd564d567f4a22e0969c0de7a07fa6adcd4e
Vicente <vicente.brevis@usach.cl>
2019-06-15 23:02:55 -0400

    Se agrega test ClienteRepository

--- src/test/java/MingesoTingeso/demo/Repositories/ClienteRepositoryTest.java
+++ src/test/java/MingesoTingeso/demo/Repositories/ClienteRepositoryTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import MingesoTingeso.demo.Models.Cliente;
 import static org.junit.Assert.*;
-/**
+
 @RunWith( SpringJUnit4ClassRunner.class )
 @ContextConfiguration
 @SpringBootTest
@@ -21,8 +21,8 @@ public class ClienteRepositoryTest {
 	public void findClienteByIdCliente() {
 		long idTest = 1;
 		Cliente c = clienteRepository.findClienteByIdCliente(idTest);
-		c.setNombreCliente("Juan Perez");
-		assertEquals("Juan Perez", c.getNombreCliente());
+		long comp = c.getIdCliente();
+		assertEquals(idTest, comp);
 	}
 
 	@Test
@@ -31,4 +31,4 @@ public class ClienteRepositoryTest {
 		assertEquals(18123456,c.getRut());
 
 	}
-}**/
+}
