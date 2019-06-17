c96facc57c0cd7e23249920c5d087890eb039cdb
Vicente <vicente.brevis@usach.cl>
2019-06-17 02:23:27 -0400

    Se corrige RegistroTest

--- src/test/java/MingesoTingeso/demo/Models/RegistroTest.java
+++ src/test/java/MingesoTingeso/demo/Models/RegistroTest.java
@@ -203,4 +203,23 @@ public class RegistroTest {
         }
     }
 
+    @Test
+    public void setPrecio() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String dateInString1 = "2019-05-05";
+        String dateInString2 = "2019-05-10";
+        try {
+            Date fechaInicio = formatter.parse(dateInString1);
+            Date fechaTermino = formatter.parse(dateInString2);
+            Habitacion h = new Habitacion("Simple",101,2,2,6000);
+            Registro r = new Registro("Edgar Blau",fechaInicio,fechaTermino, h);
+            h = new Habitacion("Simple",1112,2,2,8000);
+            int xd = 8500;
+            r.setPrecio(xd);
+            assertEquals(java.util.Optional.of(xd),r.getPrecio());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
