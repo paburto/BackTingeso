0c7571e5850050446b451e78fb5fda18eccd5ba4
Vicente <vicente.brevis@usach.cl>
2019-06-16 21:53:32 -0400

    Se agrega test para historial

--- src/test/java/MingesoTingeso/demo/Models/HistorialTest.java
+++ src/test/java/MingesoTingeso/demo/Models/HistorialTest.java
@@ -1,37 +1,37 @@
 package MingesoTingeso.demo.Models;
 
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-/**
+
 public class HistorialTest {
 
     @Test
     public void getIdHistorial() {
-      Historial h = new Historial("Lorem");
-      h.setIdHistorial(1L);
-      assertEquals(1L,(long)h.getIdHistorial());
+        Historial h = new Historial("Lorem");
+        h.setIdHistorial(1L);
+        assertEquals(1L,(long)h.getIdHistorial());
     }
 
     @Test
     public void setIdHistorial() {
-      Historial h = new Historial("Lorem");
-      h.setIdHistorial(1L);
-      assertEquals(1L,(long)h.getIdHistorial());
+        Historial h = new Historial("Lorem");
+        h.setIdHistorial(1L);
+        assertEquals(1L,(long)h.getIdHistorial());
     }
 
     @Test
     public void getDescripcionHistorial() {
-      Historial h = new Historial("Lorem");
-      assertEquals("Lorem",h.getDescripcionHistorial());
+        Historial h = new Historial("Lorem");
+        assertEquals("Lorem",h.getDescripcionHistorial());
     }
 
     @Test
     public void setDescripcionHistorial() {
-      Historial h = new Historial("Lorem");
-      h.setDescripcionHistorial("Lorem2");
-      assertEquals("Lorem2",h.getDescripcionHistorial());
+        Historial h = new Historial("Lorem");
+        h.setDescripcionHistorial("Lorem2");
+        assertEquals("Lorem2",h.getDescripcionHistorial());
     }
 
 }
-**/
