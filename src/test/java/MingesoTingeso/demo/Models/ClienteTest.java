70b30a836bd0632aa9b673b7d3db43ff6ebc7a83
Vicente <vicente.brevis@usach.cl>
2019-06-16 23:58:30 -0400

    Se agregan modificaciones a tests

--- src/test/java/MingesoTingeso/demo/Models/ClienteTest.java
+++ src/test/java/MingesoTingeso/demo/Models/ClienteTest.java
@@ -2,12 +2,19 @@
 package MingesoTingeso.demo.Models;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
 import static org.junit.Assert.*;
-
+@RunWith( SpringJUnit4ClassRunner.class )
+@ContextConfiguration
+@SpringBootTest
 public class ClienteTest {
 
     @Test
--- src/test/java/MingesoTingeso/demo/Models/ServicioTest.java
+++ src/test/java/MingesoTingeso/demo/Models/ServicioTest.java
@@ -3,8 +3,15 @@ package MingesoTingeso.demo.Models;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
+@RunWith( SpringJUnit4ClassRunner.class )
+@ContextConfiguration
+@SpringBootTest
 public class ServicioTest {
 
     @Test
